2d3ddf083bc4083c8d5b54bde3d4b57c0a7f20c7
emmanueil1 <emmanueiljames@gmail.com>
2023-08-23 10:22:48 +0500

    First Time Assigmnment Created

---
+++ Assign1.js
@@ -0,0 +1,36 @@
+"use strict";
+// Assignment No.1
+// Q1 - Print a message like "Hey Abu Hurairah, I have started learning TYPESCRIPT."
+// Q2 - Store your name in a variable and print it.
+// Q3 - Store 10 numbers in different variables.
+// Q4 - Add all of them and print the SUM.
+// Q5 - Print the Difference (subtraction).
+// Q6 - Print the result after multiplying all.
+// Q7 - Take two numbers and print the division result.
+// Q8 - Now, perform all four operations with the given numbers, print the results, and observe the order in which the operations take place.
+// Q1 - Print a message like "Hey Abu Hurairah, I have started learning TYPESCRIPT."
+console.log("Hey Abu Hurairah, I have started learning TYPESCRIPT.");
+// Q2 - Store your name in a variable and print it
+var student = "Emmanueil";
+console.log(student);
+// Q3 - Store 10 numbers in different variables.
+var num1 = 1;
+var num2 = 2;
+var num3 = 3;
+var num4 = 4;
+var num5 = 5;
+var num6 = 6;
+var num7 = 7;
+var num8 = 8;
+var num9 = 9;
+var num10 = 10;
+// Q4 - Add all of them and print the SUM.
+console.log("SUM =", num1 + num2 + num3 + num4 + num5 + num6 + num7 + num8 + num9 + num10);
+// Q5 - Print the Difference (subtraction).
+console.log("Subtraction =", num1 - num2 - num3 - num4 - num5 - num6 - num7 - num8 - num9 - num10);
+// Q6 - Take two numbers and print the division result.
+console.log("Division Result =", num10 / num3);
+// Q7 - Print the result after multiplying all.
+console.log("Result Of Multiplication =", num1 * num2 * num3 * num4 * num5 * num6 * num7 * num8 * num9 * num10);
+// Q8 - Now, perform all four operations with the given numbers, print the results, and observe the order in which the operations take place.
+console.log("Results =", num1 * num2 + num3 - num4 / num5 - num6 + num7 / num8 * num9 + num10);
